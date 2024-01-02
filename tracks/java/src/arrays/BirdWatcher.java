5c105a582857009d288a616a4fa399accf75fa24
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-01-02 06:44:30 +0600

    Java Bird Watcher problem solved

---
+++ tracks/java/src/arrays/BirdWatcher.java
@@ -0,0 +1,53 @@
+package arrays;
+
+public class BirdWatcher {
+    private final int[] birdsPerDay;
+
+    public BirdWatcher(int[] birdsPerDay) {
+        this.birdsPerDay = birdsPerDay;
+    }
+
+    public int[] getLastWeek() {
+        return birdsPerDay;
+    }
+
+    public int getToday() {
+        return birdsPerDay.length == 0 ? 0 : birdsPerDay[birdsPerDay.length -1];
+    }
+
+    public int incrementTodaysCount() {
+        int idx = birdsPerDay.length - 1;
+        birdsPerDay[idx] = ++birdsPerDay[idx];
+        return birdsPerDay[idx];
+    }
+
+    public boolean hasDayWithoutBirds() {
+        for (int i = 0; i < birdsPerDay.length; i++) {
+            if (birdsPerDay[i] == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int getCountForFirstDays(int numberOfDays) {
+        int result = 0;
+
+        if (numberOfDays > birdsPerDay.length) {
+            for (int i = 0; i < birdsPerDay.length; i++) {
+                result += birdsPerDay[i];
+            }
+            return result;
+        }
+
+        for (int i = 0; i < numberOfDays; i++) {
+            result += birdsPerDay[i];
+        }
+
+        return result;
+    }
+
+    public int getBusyDays() {
+        throw new UnsupportedOperationException("Please implement the BirdCount.getBusyDays() method");
+    }
+}
