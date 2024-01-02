f79e6ba6c97356e9409166bfa393ae70a9e2c9af
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-01-02 09:29:20 +0600

    Java Language List completed

---
+++ tracks/java/src/List/LanguageList.java
@@ -0,0 +1,36 @@
+package List;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LanguageList {
+    private final List<String> languages = new ArrayList<>();
+
+    public boolean isEmpty() {
+        return languages.isEmpty();
+    }
+
+    public void addLanguage(String language) {
+        languages.add(language);
+    }
+
+    public void removeLanguage(String language) {
+        languages.remove(languages.indexOf(language));
+    }
+
+    public String firstLanguage() {
+        return languages.get(0);
+    }
+
+    public int count() {
+        return languages.size();
+    }
+
+    public boolean containsLanguage(String language) {
+        return languages.contains(language);
+    }
+
+    public boolean isExciting() {
+        return this.containsLanguage("Java") || this.containsLanguage("Kotlin");
+    }
+}
