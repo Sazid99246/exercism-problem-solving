590150a50a299082470272f1abad1b046586f61f
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-01-07 22:59:44 +0600

    Java Prime Factor problem solved

---
+++ tracks/java/src/PrimeFactorsCalculator.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The {@code PrimeFactorsCalculator} class provides a method to calculate the
+ * prime factors of a given number.
+ */
+public class PrimeFactorsCalculator {
+
+    /**
+     * Calculates and returns the list of prime factors of the specified number.
+     * 
+     * @param number the number to find the prime factors of.
+     * @return a list of long integers representing the prime factors of the given
+     *         number.
+     *         If the number is less than 2, returns an empty list.
+     */
+    List<Long> calculatePrimeFactorsOf(long number) {
+        List<Long> listResult = new ArrayList<>();
+
+        // Return empty list if number is less than 2
+        if (number < 2L)
+            return listResult;
+
+        // Add number itself if it is 2 (which is a prime number)
+        if (number == 2L) {
+            listResult.add(number);
+            return listResult;
+        }
+
+        // Start checking for factors from 2 onwards
+        long i = 2L;
+        while (number >= i) {
+            if (number % i == 0) {
+                listResult.add(i);
+                number /= i;
+            } else {
+                i++;
+            }
+        }
+
+        return listResult;
+    }
+}
