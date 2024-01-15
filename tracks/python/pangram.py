5e6f4dfadb1226c91d0b90a0ee6944077c13a7c3
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-01-15 23:17:42 +0600

    Python pangram problem solved

---
+++ tracks/python/pangram.py
@@ -0,0 +1,7 @@
+def is_pangram(sentence):
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+               'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    for letter in letters:
+        if letter not in sentence.lower():
+            return False
+    return True
