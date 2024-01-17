db825cb0fb9c7ccc417d3c87d664eeaf48ffca6d
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-01-17 09:38:31 +0600

    JavaScript Hello World problem solved

---
+++ tracks/javascript/hello-world.js
@@ -0,0 +1,12 @@
+//
+// This is only a SKELETON file for the 'Hello World' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+/**
+ * Return a greeting string.
+ * @returns A greeting string 'Hello, World!
+ */
+export function hello() {
+    return 'Hello, World!';
+}  
