a0e2f7d209826912e6e0a9bc8a7020b58e0ffb0b
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-01-17 13:15:03 +0600

    PHP Lasagna problem solved

---
+++ tracks/php/Lasagna.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+class Lasagna
+{
+    function expectedCookTime()
+    {
+        return 40;
+    }
+
+    function remainingCookTime($elapsed_minutes)
+    {
+        return 40 - $elapsed_minutes;
+    }
+
+    function totalPreparationTime($layers_to_prep)
+    {
+        return 2 * $layers_to_prep;
+    }
+
+    function totalElapsedTime($layers_to_prep, $elapsed_minutes)
+    {
+        return $this->totalPreparationTime($layers_to_prep) + $elapsed_minutes;
+    }
+
+    function alarm()
+    {
+        return 'Ding!';
+    }
+}
