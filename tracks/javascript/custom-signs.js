04fd9a1ee75d643f805300e6b04b069bec8bee53
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-01-27 23:03:06 +0600

    Typescript Resistor Color problem solved

--- tracks/javascript/custom-signs.js
+++ tracks/javascript/custom-signs.js
@@ -1,9 +1,3 @@
-// @ts-check
-
-// The line above enables type checking for this file. Various IDEs interpret
-// the @ts-check directive. It will give you helpful autocompletion when
-// implementing this exercise.
-
 /**
  * Build a sign that includes both of the parameters.
  *
---
+++ tracks/typescript/resistor-color.ts
@@ -0,0 +1,5 @@
+export const colorCode = (color:string):Number => {
+    return COLORS.indexOf(color);
+}
+  
+export const COLORS = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white',];
