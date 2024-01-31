752ce33ca90f24f8f4789400fdd144090ed084f0
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-01-31 09:04:12 +0600

    R Raindrop problem solved

---
+++ tracks/R/raindrops.R
@@ -0,0 +1,16 @@
+raindrops <- function(number) {
+    result = ""
+    if (number %% 3 == 0) {
+       result = paste(result, "Pling", sep = "")
+    }
+    if(number %% 5 == 0){
+        result <-paste(result,"Plang", sep = "")
+    }
+    if(number %% 7 == 0){
+        result <- paste(result,"Plong", sep="")
+    }
+    if(result == ""){
+        result <- as.character(number)
+    }
+    return(result)
+}
