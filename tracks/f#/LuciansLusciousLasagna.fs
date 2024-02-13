cd684e5abc0c4cbc1016b5b356e450257ec637dd
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-02-13 09:18:17 +0600

    F# lasagna problem sovled

---
+++ tracks/f#/LuciansLusciousLasagna.fs
@@ -0,0 +1,9 @@
+module LuciansLusciousLasagna
+
+let expectedMinutesInOven = 40;
+
+let remainingMinutesInOven minutesInOven = expectedMinutesInOven - minutesInOven
+
+let preparationTimeInMinutes layerCount = layerCount * 2
+
+let elapsedTimeInMinutes layerCount minutesInOven = preparationTimeInMinutes layerCount + minutesInOven
